39839042ef4d285c9001411677f79843823db23b
Daoqi Wang <dwang089@gmail.com>
2018-04-14 00:44:20 -0700

    Adding APIs for transactions

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const P2PServer = require('./p2p-server');
+const Wallet = require('./wallet');
+const TransactionPool = require('./transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
 const p2pServer = new P2PServer(bc);
+const wallet = new Wallet();
+const TxnPool = new TransactionPool()
 
 app.use(bodyParser.json());
 
@@ -22,6 +26,16 @@ app.post('/mine', (req, res) => {
 
   p2pServer.syncChain();
   res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+  res.json(TxnPool.transactions);
+});
+
+app.post('/transaction', (req, res) => {
+  const { receiver, amount } = req.body;
+  const transaction = wallet.newTransaction(receiver, amount, TxnPool);
+  res.redirect('transactions');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
